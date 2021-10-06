a71afdeb3b4bf1307f19df56c12d50f236c545e4
Philipp Storz <philipp.storz@bareos.com>
2021-10-06 15:43:40 +0200

    python-fd-module-tester.cc: use inttypes macro for formatting

--- core/src/plugins/filed/python/test/python-fd-module-tester.cc
+++ core/src/plugins/filed/python/test/python-fd-module-tester.cc
@@ -100,8 +100,8 @@ bRC bareosJobMsg(PluginContext* ctx,
                  const char* fmt,
                  ...)
 {
-  printf("bareosJobMsg file:%s line:%d type:%d time: %ld, fmt:%s\n", file, line,
-         type, (int64_t)mtime, fmt);
+  printf("bareosJobMsg file:%s line:%d type:%d time: %+" PRId64 ", fmt:%s\n",
+         file, line, type, (int64_t)mtime, fmt);
   return bRC_OK;
 };
 bRC bareosDebugMsg(PluginContext* ctx,
